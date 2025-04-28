e4802863891d0a95be7dfefeb0799899a6004216
im-varun <vrun.mulchandani@gmail.com>
2025-04-28 11:53:43 -0500

    Add values topic

---
+++ 02-values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("2 + 3 =", 2+3)
+
+	fmt.Println("7.0 / 3 =", 7.0/3)
+	fmt.Println("7.0 / 3.0 =", 7.0/3.0)
+
+	fmt.Println("6.0 / 3 =", 6.0/3)
+	fmt.Println("6.0 / 3.0 =", 6.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!false)
+
+	fmt.Println("True =", true)
+	fmt.Println("False =", false)
+}
