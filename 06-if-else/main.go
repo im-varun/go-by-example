67c1243eacfa1a4b3e12da8f8adc2350b14c6ca4
im-varun <vrun.mulchandani@gmail.com>
2025-04-29 18:57:21 -0500

    Add if-else topic

---
+++ 06-if-else/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 4%2 == 0 {
+		fmt.Println("4 is even")
+	} else {
+		fmt.Println("4 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if 8%2 == 0 || 7%2 == 0 {
+		fmt.Println("either 8 is even or 7 is even")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
