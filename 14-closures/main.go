c7f589580c683941aaaaebaafb479efb8e85359d
im-varun <vrun.mulchandani@gmail.com>
2025-05-01 21:20:53 -0500

    Add simple anonymous function example

--- 14-closures/main.go
+++ 14-closures/main.go
@@ -23,4 +23,19 @@ func main() {
 	seq3 := intSeq()
 	fmt.Println(seq3())
 	fmt.Println(seq3())
+
+	var sum func(nums ...int) int
+	sum = func(nums ...int) int {
+		total := 0
+
+		for _, num := range nums {
+			total += num
+		}
+
+		return total
+	}
+
+	fmt.Println("1 + 2 =", sum(1, 2))
+	fmt.Println("1 + 2 + 3 =", sum(1, 2, 3))
+	fmt.Println("1 + 2 + 3 + 4 =", sum(1, 2, 3, 4))
 }
